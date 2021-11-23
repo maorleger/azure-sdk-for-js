78fbdbf0a4b7d73f886c735526731acb219fba29
Maor Leger <malege@microsoft.com>
2021-11-23 14:16:20 -0800

    wip: just spiking out some of the details

---
+++ sdk/instrumentation/instrumentation-opentelemetry/test/internal/inMemoryInstrumenter.ts
@@ -0,0 +1,119 @@
+import {
+  Instrumenter,
+  InstrumenterSpanOptions,
+  SpanStatus,
+  TracingContext,
+  TracingSpan,
+  TracingSpanContext
+} from "@azure/core-tracing";
+
+export class InMemoryTracingSpan implements TracingSpan {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  status?: SpanStatus;
+  attributes: Record<string, unknown> = {};
+  endCalled: boolean = false;
+  exception?: string | Error;
+  setStatus(status: SpanStatus): void {
+    this.status = status;
+  }
+  setAttribute(name: string, value: unknown): void {
+    this.attributes[name] = value;
+  }
+  end(): void {
+    this.endCalled = true;
+  }
+  recordException(exception: string | Error): void {
+    this.exception = exception;
+  }
+  isRecording(): boolean {
+    return true;
+  }
+  get spanContext() {
+    return {
+      spanId: "",
+      traceFlags: 0,
+      traceId: ""
+    };
+  }
+}
+
+export class InMemoryInstrumenter implements Instrumenter {
+  public contexts: TracingContext[] = [new ContextImpl()];
+  public startedSpans: InMemoryTracingSpan[] = [];
+
+  startSpan(
+    name: string,
+    spanOptions?: InstrumenterSpanOptions
+  ): { span: TracingSpan; tracingContext: TracingContext } {
+    const span = new InMemoryTracingSpan(name);
+    let context: TracingContext = new ContextImpl(spanOptions?.tracingContext);
+    context = context.setValue(Symbol.for("span"), span);
+    this.startedSpans.push(span);
+    return { span, tracingContext: context };
+  }
+  withContext<
+    CallbackArgs extends unknown[],
+    Callback extends (...args: CallbackArgs) => ReturnType<Callback>
+  >(
+    context: TracingContext,
+    callback: Callback,
+    _callbackThis?: ThisParameterType<Callback>,
+    ..._callbackArgs: CallbackArgs
+  ): ReturnType<Callback> {
+    return this.withContextInternal(context, callback) as any;
+  }
+  async withContextInternal<
+    TReturn,
+    CallbackArgs extends unknown[],
+    Callback extends (...args: CallbackArgs) => Promise<TReturn>
+  >(
+    context: TracingContext,
+    callback: Callback,
+    callbackThis?: ThisParameterType<Callback>,
+    ...callbackArgs: CallbackArgs
+  ): Promise<any> {
+    this.contexts.push(context);
+    try {
+      return await callback.call(callbackThis, ...callbackArgs);
+    } finally {
+      this.contexts.pop();
+    }
+  }
+  parseTraceparentHeader(_traceparentHeader: string): TracingSpanContext | undefined {
+    return;
+  }
+  createRequestHeaders(_spanContext: TracingSpanContext): Record<string, string> {
+    return {};
+  }
+  currentContext() {
+    return this.contexts[this.contexts.length - 1];
+  }
+}
+
+export class ContextImpl implements TracingContext {
+  private contextMap: Map<Symbol, unknown>;
+
+  constructor(parentContext?: TracingContext) {
+    if (parentContext && !(parentContext instanceof ContextImpl)) {
+      throw new Error("received parent context, but it is not mock context...");
+    }
+    this.contextMap = new Map(parentContext?.contextMap || new Map());
+  }
+
+  setValue(key: symbol, value: unknown): TracingContext {
+    const newContext = new ContextImpl(this);
+    newContext.contextMap.set(key, value);
+    return newContext;
+  }
+  getValue(key: symbol): unknown {
+    return this.contextMap.get(key);
+  }
+  deleteValue(key: symbol): TracingContext {
+    const newContext = new ContextImpl(this);
+    newContext.contextMap.delete(key);
+    return newContext;
+  }
+}
---
+++ sdk/instrumentation/instrumentation-opentelemetry/test/internal/works.spec.ts
@@ -0,0 +1,68 @@
+import chai, { assert } from "chai";
+import { chaiAzureTrace } from "./chai";
+chai.use(chaiAzureTrace);
+import { InMemoryInstrumenter } from "./inMemoryInstrumenter";
+import { createTracingClient, TracingClient, useInstrumenter } from "@azure/core-tracing";
+// https://github.com/maorleger/azure-sdk-for-js/compare/instrumentation-opentelemetry...maorleger:spike-instrumenter-tests?expand=1
+
+// So like we can have some low-level support with ways to inspect the instrumenter
+// check the spans, etc.
+describe("in-memory", () => {
+  const instrumenter = new InMemoryInstrumenter();
+
+  beforeEach(() => {
+    useInstrumenter(instrumenter);
+  });
+
+  it("works", async () => {
+    const instrumenter = new InMemoryInstrumenter();
+    const { span, tracingContext } = instrumenter.startSpan("foo");
+    console.log(span);
+    console.log(tracingContext);
+
+    await instrumenter.withContext(tracingContext, () => {
+      assert.equal(instrumenter.currentContext(), tracingContext);
+      return Promise.resolve(5);
+    });
+    span.end();
+
+    assert.equal(instrumenter.startedSpans.length, 1);
+    assert.isTrue(instrumenter.startedSpans[0].endCalled);
+
+    assert.notEqual(instrumenter.currentContext(), tracingContext);
+  });
+});
+
+// We can also offer some high-level "supports tracing" using these InMemoryInstrumenters
+// You can see maybe https://github.com/Azure/azure-sdk-for-js/blob/main/sdk/keyvault/keyvault-common/test/utils/supportsTracing.ts for
+// inspiration?
+// Ideally this is the only test package authors would need to write
+describe.only("high-level support", () => {
+  it("supports tracing", async () => {
+    await assert.supportsTracing(async () => {
+      // Right now it has to be initialized here so that the
+      // useInstrumenter call is called first
+      const client = new FakeClientToTest();
+      client.fakeMethod();
+    });
+  });
+});
+
+// Pretend this is a real client with real methods - like QueryLogsClient
+// or something that has upgraded to core-tracing preview.14
+class FakeClientToTest {
+  tracingClient: TracingClient;
+  constructor() {
+    this.tracingClient = createTracingClient({
+      namespace: "Microsoft.Test",
+      packageInformation: {
+        name: "@azure/test",
+        version: "foobar"
+      }
+    });
+  }
+
+  async fakeMethod() {
+    return this.tracingClient.withSpan("test", () => {});
+  }
+}
