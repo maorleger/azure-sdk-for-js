57e8bb869d00a5f9e43c67bcce63c1ad02d0923b
Maor Leger <maorleger@microsoft.com>
2024-11-18 18:28:02 +0000

    wip

--- sdk/keyvault/generate.ts
+++ sdk/keyvault/generate.ts
@@ -46,5 +46,8 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 delete packageJson.dependencies["@azure/core-client"];
 packageJson.dependencies["@azure-rest/core-client"] = "^2.0.0";
 
+// Update @azure/core-lro to 3.0.0
+packageJson.dependencies["@azure/core-lro"] = "^3.0.0";
+
 // Write updated package.json back to disk
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), "utf8");
